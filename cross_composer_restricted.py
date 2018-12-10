24bcd69faee2c53df65decb25e22fbefbe062fd1
Emirhan Karagül <emirhan350z@gmail.com>
2018-12-11 00:22:50 +0300

    Fix major bug in csv processing

--- cross_composer_restricted.py
+++ cross_composer_restricted.py
@@ -104,7 +104,10 @@ def get_inputs_for_rnn(artist, song, transpose_offset):
         for row in chordreader:
             if song_found == True:
                 if row[0] == '':
-                    if float(row[1]) >= chords[index][0] and index != len(chords) - 1:
+                    if index == len(chords) - 1:
+                        chords[index][2] = [val/np.sum(chords[index][2]) for val in chords[index][2]]
+                        chords[index][2] = np.roll(chords[index][2] , transpose_offset)
+                    elif float(row[1]) >= chords[index + 1][0]:
                         if (np.sum(chords[index][2])) != 0:
                             chords[index][2] = [val/np.sum(chords[index][2]) for val in chords[index][2]]
                             chords[index][2] = np.roll(chords[index][2] , transpose_offset)
@@ -153,143 +156,12 @@ def key_name_extractor(row):
     else:
         return m.group(0)
     
-
-    
-#%%
-try:
-    songs = pd.read_pickle("./cross_composer_features_restricted.pkl")
-except FileNotFoundError:
-    songs = pd.read_csv('./cross-composer_annotations.csv')
-    songs['key_string'] = songs.apply(lambda row: key_name_extractor(row),axis=1)
-    songs = songs[songs['key_string'] != ""]
-    songs['mode'] = songs.apply(lambda row: key_mode_extractor(row),axis=1)
-    songs['key'] = songs.apply(lambda row: key_number_extractor(row),axis=1)
-    songs['features'] = songs.apply(lambda row: get_inputs_for_rnn(row.Class, row.Filename, -1 * row.key), axis = 1)
-    songs['length'] = songs.apply(lambda row: len(row.features), axis = 1)
-    songs = songs[songs['length'] > 10 ]
-    songs.to_pickle("./cross_composer_features.pkl")
-#%%
-class KerasBatchGenerator(object):
-    
-    def __init__(self, df, num_steps, batch_size, skip_step=5):
-        self.df = df
-        self.data = df.iloc[0].features
-        self.mode = df.iloc[0]['mode']
-        self.class_num = int(df.iloc[0]['Class'][:2]) - 1
-        self.num_steps = num_steps
-        self.batch_size = batch_size
-        self.song_idx = 0
-        self.current_idx = 0
-        self.skip_step = skip_step
-        self.mode_features = self.get_mode_array()
-        #self.class_features = self.get_class_array()
-        
-    def get_mode_array(self):
-        if self.mode == 1:
-            mode_arr = np.array([[0,1]])
-        else:
-            mode_arr = np.array([[1,0]])
-        result = np.array(mode_arr)
-        for i in range(self.num_steps - 1):
-            result = np.concatenate((result,mode_arr),axis=0)
-        return result
-        
-    def generate(self):
-        x = np.zeros((self.batch_size, self.num_steps, 14))
-        y = np.zeros((self.batch_size, self.num_steps, 25))
-        while True:
-            for i in range(self.batch_size):
-                if self.current_idx + self.num_steps >= len(self.data):
-                    # reset the index back to the start of the data set
-                    self.current_idx = 0
-                    self.song_idx += 1
-                    if(len(self.df) == self.song_idx):
-                        self.song_idx = 0
-                    self.data = self.df.iloc[self.song_idx].features
-                    self.mode = self.df.iloc[self.song_idx]['mode']
-                    self.mode_features = self.get_mode_array()
-                    #self.class_features = self.get_class_array()
-                #temp_x = np.concatenate((np.concatenate(((np.array([x_samp[2] for x_samp in self.data[self.current_idx:self.current_idx + self.num_steps]])>0.15).astype(int),self.mode_features), axis = 1),self.class_features), axis = 1)
-                temp_x = np.concatenate(([x_samp[2] for x_samp in self.data[self.current_idx:self.current_idx + self.num_steps]],self.mode_features), axis = 1)
-                temp_x[temp_x < 0.12] = 0
-                x[i, :, :] = temp_x
-                temp_y = [y_samp[1] for y_samp in self.data[self.current_idx :self.current_idx + self.num_steps ]]
-                # convert all of temp_y into a one hot representation
-                y[i, :, :] = to_categorical(temp_y, num_classes = 25)
-                self.current_idx += self.skip_step
-            yield x, y
-            
-#%%
-class KerasBatchGeneratorModeless(object):
-    
-    def __init__(self, df, num_steps, batch_size, skip_step=5):
-        self.df = df
-        self.data = df.iloc[0].features
-        self.class_num = int(df.iloc[0]['Class'][:2]) - 1
-        self.num_steps = num_steps
-        self.batch_size = batch_size
-        self.song_idx = 0
-        self.current_idx = 0
-        self.skip_step = skip_step
-        
-    def generate(self):
-        x = np.zeros((self.batch_size, self.num_steps, 12))
-        y = np.zeros((self.batch_size, self.num_steps, 25))
-        while True:
-            for i in range(self.batch_size):
-                if self.current_idx + self.num_steps >= len(self.data):
-                    self.current_idx = 0
-                    self.song_idx += 1
-                    if(len(self.df) == self.song_idx):
-                        self.song_idx = 0
-                    self.data = self.df.iloc[self.song_idx].features
-                temp_x = [x_samp[2] for x_samp in self.data[self.current_idx:self.current_idx + self.num_steps]]
-                x[i, :, :] = temp_x
-                temp_y = [y_samp[1] for y_samp in self.data[self.current_idx :self.current_idx + self.num_steps ]]
-                # convert all of temp_y into a one hot representation
-                y[i, :, :] = to_categorical(temp_y, num_classes = 25)
-                self.current_idx += self.skip_step
-            yield x, y
-            
-#%%
-#Create training and testing samples
-msk = np.random.rand(len(songs)) 
-
-train = songs[msk < 0.7]
-valid = songs[np.logical_and(msk < 0.85, msk >= 0.7)]
-test = songs[msk >= 0.85]
-#%%
-num_steps = 4
-hidden_size = 256
-batch_size = 20
-num_epochs = 10
-results=[]
-dropout = 0.5
-#%%
-    
-for hidden_size in [64,128,256]:
-    print('training for hidden 3 layered : ' +str(hidden_size) +' dropout: ' + str(dropout) + ' num_steps: '+ str(num_steps))
-    train_data_generator = KerasBatchGenerator(train, num_steps, batch_size, skip_step=1)
-    validation_data_generator = KerasBatchGenerator(valid, num_steps, batch_size, skip_step=1)
-    test_data_generator = KerasBatchGenerator(test, num_steps, batch_size, skip_step=1)
-    
-    model = Sequential()
-    model.add(Dense(hidden_size,input_shape=(num_steps,14)))
-    model.add(LSTM(hidden_size, return_sequences=True))
-    model.add(BatchNormalization())
-    model.add(Dropout(dropout))
-    model.add(LSTM(hidden_size, return_sequences=True))
-    model.add(Dropout(dropout))
-    model.add(TimeDistributed(Dense(25)))
-    model.add(Activation('softmax'))
-    
-    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['categorical_accuracy'])
-    checkpointer = ModelCheckpoint(filepath='./training_checkpoints' + '/model-normalized-{epoch:02d}'+'steps=' + str(num_steps)+'hidden='+str(hidden_size) +'dropout='+ str(dropout)+ '.hdf5', verbose=2, period=10)
-    
-    model.fit_generator(train_data_generator.generate(), (sum(train['length'])-train.shape[0] * num_steps)//(batch_size), num_epochs,
-                        validation_data=validation_data_generator.generate(),
-                        validation_steps=(sum(valid['length'])-valid.shape[0] * num_steps)//(batch_size),
-                        callbacks=[checkpointer],verbose=1)
-    
-    scores = model.evaluate_generator(test_data_generator.generate(), steps=(sum(test['length']) - test.shape[0]*num_steps)//(batch_size), verbose=1)
-    results.append(scores[1])
+songs = pd.read_csv('./cross-composer_annotations.csv')
+songs['key_string'] = songs.apply(lambda row: key_name_extractor(row),axis=1)
+songs = songs[songs['key_string'] != ""]
+songs['mode'] = songs.apply(lambda row: key_mode_extractor(row),axis=1)
+songs['key'] = songs.apply(lambda row: key_number_extractor(row),axis=1)
+songs['features'] = songs.apply(lambda row: get_inputs_for_rnn(row.Class, row.Filename, -1 * row.key), axis = 1)
+songs['length'] = songs.apply(lambda row: len(row.features), axis = 1)
+songs = songs[songs['length'] > 10 ]
+songs.to_pickle("./cross_composer_features_restricted.pkl")
